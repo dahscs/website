b687e87994c04ff9779e76d1168abd905801eb9d
Edelio Taboada <90776613+Edelio-Taboada@users.noreply.github.com>
2022-08-02 13:35:04 -0400

    Image Tab, footer

--- functions.js
+++ functions.js
@@ -1,15 +1,28 @@
 /*main page picture functionality */
-let clubPicsArr = ["Pictures/clubPicture2022.jpeg",
+let clubPicsArr = ["Pictures/clubPicture2022.jpg",
 "Pictures/clubPicture2019.jpeg", "Pictures/clubPicture2017.jpeg"];
 
 let clubPicsArrDesc = ["Club Picture of the 2022 Year", "Club Picture of the 2019 Year", "Club Picture of the 2017 year"] ;
 let picId=0;
 function goprevious(){
     picId+=1;
-    picId= picId%clubPicsArr.length;
-    document.getElementById("clubPics").src=clubPicsArr[picId];
+    picId= picId%(clubPicsArr.length);
+    idneededshow = "clubPic";
+    idneededtohide = "clubPic";
+    if(picId==0){
+        idneededtohide += String(clubPicsArr.length);
+    }
+    else{
+        idneededtohide += String(picId);
+    }
+    idneededshow += String(picId+1);
+
+    document.getElementById(idneededshow).style.opacity="1";
+    document.getElementById(idneededtohide).style.opacity="0";
     document.getElementById("descp").innerHTML = clubPicsArrDesc[picId];
+
 }
+
 
 function goforward(){
     picId-=1;
@@ -17,6 +30,22 @@ function goforward(){
     if(picId<0){
         picId += clubPicsArr.length
     }
-    document.getElementById("clubPics").src=clubPicsArr[picId];
+
+    idneededshow = "clubPic";
+    idneededtohide = "clubPic";
+
+    if(picId==clubPicsArr.length-1){
+        idneededtohide += String(1);
+    }
+    else{
+        idneededtohide += String(picId+2);
+    }
+
+    idneededshow += String(picId+1);
+
+
+    document.getElementById(idneededshow).style.opacity="1";
+    document.getElementById(idneededtohide).style.opacity="0";
+
     document.getElementById("descp").innerHTML = clubPicsArrDesc[picId];
 }
